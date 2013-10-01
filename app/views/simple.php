9f489185d86dca3334151dcc2aab2b8e0415ebbd
ricardomorales <ric.morales22@gmail.com>
2013-10-01 14:30:24 -0400

    Third attempt to see if changes have been added.

---
+++ app/views/simple.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- app/views/simple.php -->
+
+<head>
+	<meta charset="UTF-8">
+	<title>Views!</title>
+</head>
+
+<body>
+	<p>Oh yeah! VIEWS!</p>
+</body>
+
+</html>
