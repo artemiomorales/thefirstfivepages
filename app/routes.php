5d874bea1e8fa23571b560a13ba12a5000ad92f9
ricardomorales <ric.morales22@gmail.com>
2013-10-24 21:14:55 -0400

    A quick change to welcome our new developers.

--- app/routes.php
+++ app/routes.php
@@ -11,41 +11,72 @@
 |
 */
 
+// app/routes.php
+
+
 Route::get('/', function()
 {
 	return View::make('hello');
 });
 
-Route::get('/{squirrel}', function($squirrel)
+
+Route::get('/public', function()
+{
+	$bloom = 'robots.txt';
+	$test_response = Response::download($bloom, "Awesome Information", array('Content-Type'=>'application/octet-stream'));
+	return $test_response;
+});
+
+
+Route::get('file/download', function()
+{
+
+
+});
+
+Route::filter('birthday', function()
+{
+	if (date('d/m/y') == '04/10/13') {
+		return View::make('birthday');
+	}
+});
+
+Route::filter('test', function($route, $request)
+{
+	
+});
+
+Route::filter('test', function($route, $request, $response)
+{
+	
+});
+
+
+/* Route::get('/{squirrel}', function($squirrel)
 {
 	$data['squirrel'] = $squirrel;
-	return View::make('simple', $data);
+	return View::make('hello', $data);
 });
 
-Route::get('my/page', function()
-{
-	return 'Hello world!';
+namespace Illuminate\Support\Facades;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Contracts\ArrayableInterface;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+
+$test5 = new Route();
+
+$test5::get('/test5', function(){
+	echo 'this works';
+	$response = new BinaryFileResponse('test.txt', 200, array('Content-Type'=>'application/octet-stream'), true, 'attachment');
+	
+	echo getcwd();
+
+	$monolog = Log::getMonolog();
+	$monolog->pushHandler(new \Monolog\Handler\FirePHPHandler());	
+	$monolog->addInfo($response);
+
+	return $response;
 });
 
-Route::get('first/page', function()
-{
-	return 'First!';
-});
-
-Route::get('second/page', function()
-{
-	return 'Second!';
-});
-
-Route::get('third/page', function()
-{
-	return 'Potato!';
-});
-
-Route::get('/books/{genre?}', function($genre = 'Crime')
-{
-	return "Books in the {$genre} category.";
-});
-
-// app/routes.php
-// Route::controller('account');
+*/
