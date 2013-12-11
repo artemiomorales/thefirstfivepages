383a821e8bd0bac1c0730da715844f61febcc466
ricardomorales <ric.morales22@gmail.com>
2013-12-10 22:23:52 -0500

    Updated app. Please read commit details.
    -This is a mirror image of what has been uploaded onto the live website.
    ***
    -To replicate the database, please go to PHP MyAdmin and create a blank
    database called 'thefirstfivepages'
    -After that, navigate to the thefirstfivepages folder and type in 'php
    artisan migrate'
    -Lastly, go back to PHP MyAdmin and import 'thefirstfivepages.sql' into
    the empty 'thefirstfivepages' database. 'thefirstfivepages.sql' can be
    found in the root folder.

--- prototype/head.php
+++
@@ -1,9 +0,0 @@
-<div id="header">
-	<div class="home">
-		the first five pages
-    </div>
-	
-	<div class="menu1">
-		<img src ="assets/img/mobile-menu-button.png" />
-    </div>
-</div>
--- prototype/story.php
+++
@@ -1,7 +0,0 @@
-<div id="story">
-    <div class="title">
-	    <h4>Stuff and Things</h4>
-		<h5>by Judith Barnes</h5>
-		<p></p>
-	</div>
-</div>
